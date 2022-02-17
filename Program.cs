28890d3ccf1b53f1ca729870f97f4c97501890ad
Umut Sayar <76686224+umutsa177@users.noreply.github.com>
2022-02-17 15:34:18 +0300

    Otobüs - Yolcu Projesi

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Otobüs_Yolcu
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Otobus o1 = new Otobus("Uludag", "ULUDAG", "55");
+            Otobus o2 = new Otobus("Kamilkoc", "KAMILKOC", "60");
+            Otobus o3 = new Otobus("Metro", "METRO", "50");
+
+            Yolcular y1 = new Yolcular("Vehbi", "Koc", "1");
+            Yolcular y2 = new Yolcular("Polat", "Alemdar", "12");
+            Yolcular y3 = new Yolcular("Fatma", "Ozturk", "35");
+            Yolcular y4 = new Yolcular("Turkan","Soray","23");
+
+
+            y2.Addotobüs(o1);
+            y1.Addotobüs(o3);
+            o3.Addyolcu(y4);
+            y3.Addotobüs(o2);
+
+            string fileName = "Otobus-yolcu.json";
+            string jsonString = JsonSerializer.Serialize(y1);
+            File.WriteAllText(fileName, jsonString);
+            Console.WriteLine(File.ReadAllText(fileName));
+
+            string filename = "Otobus-yolcu.json";
+            string jsonstring = JsonSerializer.Serialize(y2);
+            File.WriteAllText(filename, jsonstring);
+            Console.WriteLine(File.ReadAllText(filename));
+
+
+            string file_Name = "Otobus-yolcu.json";
+            string jsonstr = JsonSerializer.Serialize(y3);
+            File.WriteAllText(file_Name, jsonstr);
+            Console.WriteLine(File.ReadAllText(file_Name));
+
+            string asd = "Otobus-yolcu.json";
+            string qw = JsonSerializer.Serialize(y4);
+            File.WriteAllText(asd, qw);
+            Console.WriteLine(File.ReadAllText(asd));
+
+        }
+    }
+}
---
+++ yolcu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otobüs_Yolcu
+{
+    public class Yolcular
+    {
+        public string Name { get; set;}
+
+        public string Surname { get; set; }
+
+        public string Seat_no { get; set; }
+
+        public List<Otobus> Otobus { get; private set; } = new List<Otobus>();
+
+        public Yolcular(string name, string surname,string seat_no)
+        {
+            Name = name;
+            Surname = surname;
+            Seat_no = seat_no;
+        }
+
+        public Yolcular(string name, string surname, string seat_no, Otobus otobüs) : this(name, surname, seat_no)
+        {
+            otobüs.Addyolcu(this);
+
+            Otobus.Add(otobüs);
+        }
+
+        public void Addotobüs(Otobus otobüs)
+        {
+            if (Otobus.Exists(o => o.Name == otobüs.Name)) return;
+
+            Otobus.Add(otobüs);
+        }
+    }
+}
